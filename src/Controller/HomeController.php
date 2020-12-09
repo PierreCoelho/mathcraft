ace550d44deb896ec6dd2f686084755e991149ef
Pierre Coelho <p.coelho@hotmail.fr>
2020-12-09 22:52:14 +0100

    Mise en place de la page d'accueil du site web

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'crumbs' => $this->getCrumbs($request),
+        ]);
+    }
+}
